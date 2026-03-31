535d7059bdf49e80cf7030d75310d1ac1bcd669e
Jhonnatan <Jhonnatan@Mac-de-jhonnatan.local>
2026-03-31 10:32:41 -0500

    correccion adicional

--- index.php
+++ index.php
@@ -7,7 +7,27 @@
     <title>Gourmet Criollo</title>
 </head>
 <body>
-    <h1>Pagina Principal</h1>
-    
+    <div class="layout">
+        <header class="header">
+            <div class="header__contenido container">
+                <p class="header__logo">Gourmet Criollo</p>
+                <nav class="header__nav">
+                    <ul>
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Carta</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Contacto</a></li>
+                    </ul>
+                </nav>
+                <a class="cta-primary" href="#">Reservar Mesa</a>
+            </div>
+        </header>
+        <main>
+            <p>Contenido principal</p>
+        </main>
+        <footer class="footer">
+            <p>Footer</p>
+        </footer>
+    </div>
 </body>
 </html>
